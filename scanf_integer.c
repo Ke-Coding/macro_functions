b4492a6f71edaab76f38bcd382e889622cdee8dc
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-05-28 18:05:23 +0800

    scanf_integer.c
    This function can be faster than scanf.

---
+++ scanf_integer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define scanf_integer(x) {  register char tpc=getchar(), sgn=1;\
+                            for (x=0; tpc<'0' || tpc>'9'; tpc=getchar())\
+                            	if (tpc=='-')\
+                                	sgn = -1;\
+                            for (; tpc>='0' && tpc<='9'; tpc = getchar())\
+                            	x = (x<<1)+(x<<3)+tpc-'0';\
+                            x *= sgn;\
+                         }
+
+int main()
+{
+    int num;                           // The type can also be unsigned, long long, etc.
+
+    scanf_integer(num)                 // This function can be faster than scanf.
+    printf("num == %d\n", num);
+
+    return 0;
+}
