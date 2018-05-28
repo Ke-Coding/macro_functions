6a0bef7a7eea8d2673eb0c5fe47f5a5182e14b48
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-05-28 17:40:12 +0800

    split_scanf
    Scan a sentence then split it into some words.

---
+++ split_scanf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_LEN 100
+#define split_scanf(s,w,n)   scanf("%*[^a-zA-Z]");\
+                                n=0;\
+                                while(scanf("%[^ ,.?!\n]%*[ ,.?!]",s)==1)\
+                                    strcpy(w[n++],s)
+
+int main()
+{
+    int i, numOfWords = 0;
+    char sentence[MAX_LEN] = "";
+    char words[MAX_LEN][MAX_LEN] = {""};		// store the words of the sentence.
+
+
+    split_scanf(sentence, words, numOfWords);
+
+    for (i=0; i<numOfWords; i++)
+    {
+        puts(words[i]);
+    }
+
+    return 0;
+}
