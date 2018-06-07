4ef125d99bf86cb7b2cbb11a9d8e3f1905e3ab50
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-06-07 20:29:20 +0800

    scan_integer
    This function can be faster than scanf.

--- scan_integer.c
+++ scan_integer.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#define scan_integer(x)  do{  register char tpc = getchar(), sgn = 1;       \
-                            for (x=0; tpc<'0' || tpc>'9'; tpc=getchar())    \
-                            	if (tpc=='-')                                 \
-                                	sgn = -1;                                 \
-                            for (; tpc>='0' && tpc<='9'; tpc = getchar())   \
-                            	x = (x<<1)+(x<<3)+tpc-'0';                    \
-                            x *= sgn;                                       \
-                         }
+#define scan_integer(x) do{
+                            register char tpc = getchar(), sgn = 1;\
+                            for (x=0; tpc<'0' || tpc>'9'; tpc=getchar())\
+                                if (tpc=='-')\
+                                    sgn = -1;\
+                            for (; tpc>='0' && tpc<='9'; tpc = getchar())\
+                                x = (x<<1)+(x<<3)+tpc-'0';\
+                            x *= sgn;\
+                        }while(0)
 
 int main()
 {
