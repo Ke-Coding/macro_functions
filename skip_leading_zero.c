59ae4f945abe2f85ddebf5a5962624afc45ea1a8
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-06-09 01:39:24 +0800

    skip_leading_zero
    Skip the leading-zero.

--- skip_leading_zero.c
+++ skip_leading_zero.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define MAX_LEN 200
-#define skip_leading_zero() scanf("%*[^1-9]")
+#define skip_leading_zero() do { scanf("%*[^1-9]") } while(0)
 int main()
 {
     char num[MAX_LEN] = "" ;
