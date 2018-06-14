2ea7712eddc87eeaf33429ea9590f600e19fb30b
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-06-14 23:31:18 +0800

    next_array
    Generate the next-array of a string.

---
+++ next_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+#define M 500
+
+#define generNext(str,next) do {                                     \
+                                int i = 0, j = -1;                   \
+                                int len = strlen(str);               \
+                                next[0] = -1;                        \
+                                while (i<len)                        \
+                                {                                    \
+                                    if(!~j||str[i]==str[j])          \
+                                        next[++i]=++j;               \
+                                    else j=next[j];                  \
+                                }                                    \
+                            } while(0)
+int main()
+{
+	int len, i;
+	int next[M];
+	char str[M];
+	
+	scanf("%s",str);
+
+	len = strlen(str);
+	generNext(str, next);
+	for(i=0; i<len; i++)
+		printf("%d ",next[i]);
+
+    return 0;
+}
