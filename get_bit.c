35f99b2787d2d8f2b2339e9801716932b46f69a6
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-06-18 00:16:27 +0800

    get_bit
    Get each bit of a number.

--- get_bit.c
+++ get_bit.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #define get_bit(num, a) do {                                           \
-                           unsigned mask = 0x80000000;                 \
-	                       int cnt, bits = sizeof(num)*8;              \
-	                       for (cnt=0; cnt<bits ; cnt++)               \
-	                       {                                           \
-		                       a[cnt] = !!(mask>>cnt & num) +'0';      \
-	                       }                                           \
-	                           a[cnt] = 0;                             \
-	                   } while (0)
+                            unsigned mask = 0x80000000;                \
+	                    int cnt, bits = sizeof(num)*8;             \
+	                    for (cnt=0; cnt<bits ; cnt++)              \
+	                    {                                          \
+		                a[cnt] = !!(mask>>cnt & num) +'0';     \
+	                    }                                          \
+	                    a[cnt] = 0;                                \
+	                } while (0)
 
 int main()
 {
