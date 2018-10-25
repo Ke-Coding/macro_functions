061705f588c20c49dd44cb6c1175faa1cce49d40
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-10-25 18:11:56 +0800

    unroll_macro
    Unroll a macro, so that you can check it conveniently.

--- unroll_macro.c
+++ unroll_macro.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 #define MAX(x,y) (x)>(y)?((x):(y))
-#define STR(x) #x
-#define STR1(x) STR(x)
+#define __TOSTR(x) #x
+#define TOSTR(x) __TOSTR(x)
 
 int main()
 {
     int x=1, y=2, z=3;
-    char* p = STR1( MAX( MAX(x,y), z ) );
+    char* p = TOSTR( MAX( MAX(x,y), z ) );
     
     puts(p);       // ((x)>(y)?((x):(y)))>(z)?(((x)>(y)?((x):(y))):(z))
     
